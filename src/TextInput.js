7bfdb484bfbc8e9e2aa547698dc3b8bba29360d3
Evan Feenstra <evan@evanfeenstra.com>
2019-06-30 19:51:45 -0700

    state, iconz

--- src/TextInput.js
+++ src/TextInput.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { FiSend } from "react-icons/fi";
 
 class TextInput extends React.Component {
 
+  state={
+    text:'Type here...',
+  }
+
+  send = () => {
+    alert(this.state.text)
+    this.setState({text: ''})
+  }
+
   render(){
     return(<div className="text-input">
-      <input />
-      <button>></button>
+      <input value={this.state.text} 
+        onChange={e=> this.setState({text: e.target.value})}
+      />
+      <button onClick={this.send} disabled={!this.state.text}>
+        <FiSend />
+      </button>
     </div>)
   }
 
