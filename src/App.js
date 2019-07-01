783d755e590a285e90d6047accbb932bf52242ce
Evan Feenstra <evan@evanfeenstra.com>
2019-07-01 13:19:51 -0700

    working

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import './App.css'
 import coolpic from './logo.png'
 import TextInput from './TextInput'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="header">
-        <img src={coolpic} className="logo" alt="logo" />
-        Chatter
+class App extends React.Component {
+
+  state={
+    messages:[]
+  }
+
+  sendMessage = (m) => {
+    var messages = [...this.state.messages, m]
+    this.setState({messages})
+  }
+
+  render() {
+    console.log(this.state.messages)
+    return (
+      <div className="App">
+        <header className="header">
+          <img src={coolpic} className="logo" alt="logo" />
+          Chatter
       </header>
-      <TextInput />
-    </div>
-  );
+        <TextInput sendMessage={this.sendMessage} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/TextInput.js
+++ src/TextInput.js
@@ -1,24 +1,32 @@
 import React from 'react'
-import { FiSend } from "react-icons/fi";
+import { FiSend } from "react-icons/fi"; 
 
 class TextInput extends React.Component {
 
   state={
-    text:'Type here...',
+    text:"",
   }
 
   send = () => {
-    alert(this.state.text)
-    this.setState({text: ''})
+    this.props.sendMessage(this.state.text)
+    this.setState({text:""})
+  }
+
+  keyPress = (e) => {
+    if(e.key==='Enter'){
+      this.send()
+    }
   }
 
   render(){
     return(<div className="text-input">
       <input value={this.state.text} 
+        placeholder="Write your message here..."
         onChange={e=> this.setState({text: e.target.value})}
+        onKeyPress={this.keyPress}
       />
-      <button onClick={this.send} disabled={!this.state.text}>
-        <FiSend />
+      <button disabled={!this.state.text} onClick={this.send}>
+        <FiSend style={{height:15,width:15}} />
       </button>
     </div>)
   }
